ecc26a97e6da67d9aa3ce4e661897fee6459baba
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2024-06-23 21:34:38 +0300

    update files

--- ArchAfterInstallScript.sh
+++ ArchAfterInstallScript.sh
@@ -1,4 +1,12 @@
-### Install packages
+sudo pacman -Syu
+mkdir /tmp/yay && (
+	cd /tmp/yay
+	curl -OJ 'https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=yay-bin'
+	makepkg -si
+)
+
+yay -S $(cat ArchNativePackages.txt)
+yay -S $(cat ArchAurPackages.txt)
 
 chsh -s $(which zsh)
 sudo ln -sfT dash /usr/bin/sh
@@ -8,14 +16,13 @@ sudo sed -Ei 's/CriticalPowerAction=HybridSleep/CriticalPowerAction=PowerOff/' /
 sudo flatpak override --filesystem=~/.themes
 sudo cp -r /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/volnoti /usr/share/pixmaps/
 sudo cp /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/20-connectivity.conf /etc/NetworkManager/conf.d
-sudo cp /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/background.jpg /etc/lightdm
 sudo cp /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/bilal /usr/local/bin
 sudo cp /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/config.yaml /etc/interception-vimproved/
+sudo cp /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/default.conf /usr/lib/sddm/sddm.conf.d
 sudo cp /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/environment /etc
 sudo cp /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/my-kbd.yaml /etc/interception-vimproved/
 sudo cp /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/my-udevmon.yaml /etc/interception/udevmon.d
 sudo cp /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/nobeep.conf /etc/modprobe.d
-sudo cp /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/slick-greeter.conf /etc/lightdm
 sudo cp /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/smb.conf /etc/samba
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 
@@ -51,10 +58,10 @@ sudo npm install -g live-server
 
 sudo -s
 echo "kernel.sysrq = 1" >> /etc/sysctl.d/99-sysctl.conf
+echo "modules_force_load: nvidia,nvidia_modeset,nvidia_uvm,nvidia_drm" >> /etc/booster.yaml
 rm -r /root/.local/share/nvim /root/.config/nvim /root/.config/ranger /root/.local/share/ranger /root/.zshrc /root/.config/zsh /root/.themes /root/.icons /root/.fonts /root/.config/gtk-2.0 /root/.config/gtk-3.0 /root/.config/gtk-4.0
 ln -s /home/muhammad/.local/share/nvim /root/.local/share/
 ln -s /home/muhammad/.config/nvim /root/.config/
-ln -s /home/muhammad/.config/lf /root/.config/
 ln -s /home/muhammad/.config/ranger /root/.config/
 ln -s /home/muhammad/.local/share/ranger /root/.local/share/
 ln -s /home/muhammad/.zshrc /root/
