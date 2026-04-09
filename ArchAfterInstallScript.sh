0f2ce0d25dc925383ed5379a3861290cab526e72
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2026-04-09 10:35:26 +0200

    after clean install

--- ArchAfterInstallScript.sh
+++ ArchAfterInstallScript.sh
@@ -1,12 +1,12 @@
 sudo pacman -Syu
-mkdir /tmp/yay && (
-	cd /tmp/yay
-	curl -OJ 'https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=yay-bin'
-	makepkg -si
+mkdir /tmp && (
+  git clone https://aur.archlinux.org/paru.git
+  cd paru
+  makepkg -si
 )
 
-yay -S $(cat ArchNativePackages.txt)
-yay -S $(cat ArchAurPackages.txt)
+paru -S $(cat ArchNativePackages.txt)
+paru -S $(cat ArchAurPackages.txt)
 
 chsh -s $(which zsh)
 sudo ln -sfT dash /usr/bin/sh
@@ -15,7 +15,6 @@ sudo sed -Ei 's/#ParallelDownloads = 5/ParallelDownloads = 10/' /etc/pacman.conf
 sudo sed -Ei "s/#DefaultTimeoutStopSec=90s/DefaultTimeoutStopSec=3s/" /etc/systemd/system.conf
 sudo sed -Ei 's/CriticalPowerAction=HybridSleep/CriticalPowerAction=PowerOff/' /etc/UPower/UPower.conf
 sudo flatpak override --filesystem=~/.themes
-sudo cp -r /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/volnoti /usr/share/pixmaps/
 sudo cp /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/20-connectivity.conf /etc/NetworkManager/conf.d
 sudo cp /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/Arc\ Dark.ini /usr/share/albert/widgetsboxmodel/themes
 sudo cp /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/Tokyonight\ Dark.ini /usr/share/albert/widgetsboxmodel/themes
@@ -24,9 +23,8 @@ sudo cp /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/confetti /usr/l
 sudo cp /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/default.conf /usr/lib/sddm/sddm.conf.d
 sudo cp /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/environment /etc
 sudo cp /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/hyprland-minimizer /usr/local/bin
-sudo cp /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/my-kbd.yaml /etc/interception-vimproved/
+sudo cp /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/kanata.service /etc/systemd/system/kanata.service
 sudo cp /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/nobeep.conf /etc/modprobe.d
-sudo cp /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/rofi-pass-x11 /usr/local/bin
 sudo cp /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/smb.conf /etc/samba
 sudo mkdir -p /etc/pacman.d/hooks/ && sudo mv /mnt/Disk_D/Muhammad/Repositories/Arch-Backup/root_files/nvidia.hook /etc/pacman.d/hooks/
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
@@ -54,25 +52,26 @@ sudo virsh net-start default
 
 sudo systemctl enable --now auto-cpufreq
 sudo systemctl enable --now cups
+sudo systemctl enable --now kanata.service
 sudo systemctl enable --now systemd-timesyncd
-sudo systemctl enable --now tlp.service
-sudo systemctl enable --now udevmon
 sudo systemctl enable --now vnstat.service
 
-sudo npm install -g live-server
+pnpm add -g neovim live-server @google/gemini-cli
+
+flatpak install flathub io.github._0xzer0x.qurancompanion net.sapples.LiveCaptions
 
 sudo -s
 echo "kernel.sysrq = 1" >> /etc/sysctl.d/99-sysctl.conf
-echo "modules_force_load: nvidia,nvidia_modeset,nvidia_uvm,nvidia_drm" >> /etc/booster.yaml
 rm -r /root/.local/share/nvim /root/.config/nvim /root/.zshrc /root/.config/zsh /root/.themes /root/.icons /root/.fonts /root/.config/gtk-2.0 /root/.config/gtk-3.0 /root/.config/gtk-4.0
-ln -s /home/muhammad/.local/share/nvim /root/.local/share/
-ln -s /home/muhammad/.config/nvim /root/.config/
-ln -s /home/muhammad/.config/yazi /root/.config/
-ln -s /home/muhammad/.config/kanata /root/.config/
-ln -s /home/muhammad/.zshrc /root/
-ln -s /home/muhammad/.config/zsh /root/.config/
-ln -s /home/muhammad/.icons /root/
-ln -s /home/muhammad/.fonts /root/
 ln -s /home/muhammad/.config/gtk-2.0 /root/.config/
 ln -s /home/muhammad/.config/gtk-3.0 /root/.config/
 ln -s /home/muhammad/.config/gtk-4.0 /root/.config/
+ln -s /home/muhammad/.config/kanata /root/.config/
+ln -s /home/muhammad/.config/nvim /root/.config/
+ln -s /home/muhammad/.config/yazi /root/.config/
+ln -s /home/muhammad/.config/zsh /root/.config/
+ln -s /home/muhammad/.fonts /root/
+ln -s /home/muhammad/.icons /root/
+ln -s /home/muhammad/.local/share/nvim /root/.local/share/
+ln -s /home/muhammad/.themes /root/
+ln -s /home/muhammad/.zshrc /root/
